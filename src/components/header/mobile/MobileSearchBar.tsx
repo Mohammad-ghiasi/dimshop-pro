eaa988ab5c6ca7d6bda78c49070dbe84eb6572e0
Mohammad-ghiasi <mohammadghiasi005@gmail.com>
2025-04-17 20:11:49 +0330

    header-navigation-edtit-5

--- src/components/header/mobile/MobileSearchBar.tsx
+++ src/components/header/mobile/MobileSearchBar.tsx
@@ -5,7 +5,7 @@ export default function MobileSearchBar() {
   return (
     <>
     {/* آیکون سرچ سمت راست */}
-    <div className="absolute right-3 top-1/2 h-4 w-4 sm:h-5 sm:w-5 -translate-y-1/2 text-muted-foreground mt-[-4px]">
+    <div className="absolute right-3 top-1/2 h-4 w-4  -translate-y-1/2 text-muted-foreground mt-[-4px]">
     <Search size={23}  />
     </div>
 
@@ -16,7 +16,7 @@ export default function MobileSearchBar() {
     />
 
     {/* input با padding بیشتر از سمت چپ که متن به عکس نچسبه */}
-    <div  className="w-full pl-12 pr-12 sm:pl-12 sm:pr-10 rounded-full bg-input h-11 flex items-center text-[14px] text-placeholder">
+    <div  className="w-full pl-12 pr-11 rounded-full bg-input h-11 flex items-center text-[14px] text-placeholder">
         <span>جستوجو در</span>
       </div>
   </>
